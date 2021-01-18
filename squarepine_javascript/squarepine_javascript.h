8b6cc2cb2adb7d0834bb4b6a58210a0cbba40a9d
Joël R. Langlois <joel.r.langlois@gmail.com>
2021-01-18 09:52:33 -0500

    Create squarepine_javascript.h

---
+++ squarepine_javascript/squarepine_javascript.h
@@ -0,0 +1,28 @@
+#ifndef SQUAREPINE_JAVASCRIPT_H
+#define SQUAREPINE_JAVASCRIPT_H
+
+/** BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:                 squarepine_javascript
+    vendor:             SquarePine
+    version:            0.1.0
+    name:               SP Javascript Interpreter
+    description:        A decent Javascript interpreter that is trying to be compliant.
+    website:            https://www.squarepine.io
+    license:            Proprietary
+    minimumCppStandard: 11
+    dependencies:       juce_core
+
+    END_JUCE_MODULE_DECLARATION
+*/
+
+//==============================================================================
+#include <juce_core/juce_core.h>
+
+//==============================================================================
+namespace sp
+{
+    using namespace juce;
+}
+
+#endif //SQUAREPINE_JAVASCRIPT_H
